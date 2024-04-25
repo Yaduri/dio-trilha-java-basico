236226eb9b636c7634784b0b0766b74832fbd4ce
Yago Ribeiro <yaduri16@gmail.com>
2024-04-25 10:44:15 -0300

    feat: Tipos e Variaveis

--- aprendendo-sintaxe-java/src/edu/yago/tiposevariaveis/TiposVariaveis.java
+++ aprendendo-sintaxe-java/src/edu/yago/tiposevariaveis/TiposVariaveis.java
@@ -1,5 +1,20 @@
 package edu.yago.tiposevariaveis;
 
 public class TiposVariaveis {
-    
+
+    public static void main(String[] args) {
+        // Declaração de variavel
+        // Tipo + nome + valor (opcional)
+        int idade;
+        int anoFrabricaçao = 2024;
+        double salarioMinimo = 2500;
+        float pi = 3.14F; // Tipo float precisa encerrar com F
+        long longo = 123456789123L; // Precisa encerrar com L
+
+        // Para constantes
+        // final + Tipo + NOME (EM CAIXA ALTA) + valor
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+    }
 }
